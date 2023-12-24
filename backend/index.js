ada8af996f944fdc942f92c6854e0e78340aef39
yeonhub-desktop <lsy3237@gmail.com>
2023-12-24 15:34:30 +0900

    [MOD] port 변경

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 443
+const port = 80
 const cors = require('cors');
 const ExtBlockRouter = require('./routers/ExtBlock.router');
 
